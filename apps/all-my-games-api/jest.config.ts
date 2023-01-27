5c15347d3cbf4e23f115c30ea9e43695803cc2db
tnquartel <thomasquartel@outlook.com>
2023-01-27 21:32:01 +0100

    added user to api

---
+++ apps/all-my-games-api/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'all-my-games-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/all-my-games-api',
+};
