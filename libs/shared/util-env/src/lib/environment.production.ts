87c79a3de0e8668617516e037f02a8e2748c21d5
tnquartel <thomasquartel@outlook.com>
2026-02-25 21:59:36 +0100

    Add correct render url

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -3,7 +3,7 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
     production: true,
     ROOT_DOMAIN_URL: 'http://localhost:4200',
-    dataApiUrl: process.env['API_URL'] || 'https://your-app.onrender.com/api',
+    dataApiUrl: 'https://angularproject-5078.onrender.com/api',
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://thomasquartel:ThomasQua03@all-my-games.3buywbo.mongodb.net/?appName=all-my-games',
     NEO4J_URI: 'neo4j+s://6d815ad2.databases.neo4j.io',
     NEO4J_USERNAME: '6d815ad2',
